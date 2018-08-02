d6f95516bdacf1a305a369e68d8b65b00a27cd1f
Nathan Stitt <nathan@stitt.org>
2018-08-02 11:00:35 -0500

    use touch for restart

--- home/config/deploy.rb
+++ home/config/deploy.rb
@@ -44,4 +44,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets"
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-set :passenger_restart_command, 'passenger-config restart-app'
+set :passenger_restart_with_touch, true
