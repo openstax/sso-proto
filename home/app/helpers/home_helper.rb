b150c24cc583c5c53407f540bb53fa86c33e2365
Nathan Stitt <nathan@stitt.org>
2018-08-03 09:32:14 -0500

    a minimal status dashboard

--- home/app/helpers/home_helper.rb
+++ home/app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+
+  def bootstrap_data
+    {
+      user_uuid: current_user.is_anonymous? ? nil : current_user.account.uuid,
+      login_path: openstax_accounts.login_path,
+      logout_path: openstax_accounts.logout_path,
+    }.to_json
+  end
 end
--- home/app/javascript/packs/application.js
+++ home/app/javascript/packs/application.js
@@ -1,10 +1,81 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import preact, { Component, render, h } from 'preact';
+import whenReady from 'when-dom-ready';
 
-console.log('Hello World from Webpacker')
+
+const UserStatus = ({ user_uuid, login_path, logout_path }) => (
+  <div>
+    {user_uuid && <h4>Logged in as {user_uuid}</h4>}
+    <a class="button" href={user_uuid ? logout_path : login_path}>
+      {user_uuid ? "Logout" : "Login"}
+    </a>
+  </div>
+)
+
+
+const DOMAIN = 'rdls.org';
+
+const SERVERS = {
+  RailsApi: `https://rails-api.${DOMAIN}/api/status`,
+  LocalServer: `http://localhost:2899/api/status`,
+}
+
+
+const ServerError = ({ serverName, error }) => (
+  <tr>
+    <td>{serverName}</td>
+    <td class="error">⚠</td>
+    <td>{error}</td>
+  </tr>
+);
+
+const ServerStatus = ({ serverName, user_uuid, logged_in }) => {
+  return (
+    <tr>
+      <td>{serverName}</td>
+      <td class={logged_in ? 'success' : 'warning'}>{logged_in ? '👍' : '👎'}</td>
+      <td>{user_uuid}</td>
+    </tr>
+  );
+}
+
+export default class App extends Component {
+
+  state = {
+    user_uuid: this.props.user_uuid, serverStatus: [],
+  };
+
+  updateServerStatus() {
+    Promise.all(Object.keys(SERVERS).map(serverName => (
+      fetch(SERVERS[serverName], {
+        credentials: 'include',
+      })
+        .then(reply => reply.json())
+        .then(json => ({ serverName, ...json }))
+        .catch(err => ({ serverName, error: err.message }))
+    ))).then(status => {
+      this.setState({ serverStatus: status })
+    })
+  }
+
+  componentDidMount() {
+    this.updateServerStatus();
+  }
+
+  render({ login_path, logout_path }, { serverStatus, user_uuid  }) {
+    console.table(serverStatus)
+    return (
+      <div>
+        <UserStatus {...{login_path, logout_path, user_uuid} } />
+        <table>
+          {serverStatus.map(ss => ss.error ? <ServerError {...ss} /> : <ServerStatus {...ss} />)}
+        </table>
+      </div>
+    );
+  }
+}
+
+whenReady(() => {
+  const contentDiv = document.getElementById('content')
+  const bootstrap = JSON.parse(contentDiv.querySelector('script#bootstrap').innerHTML)
+  render(<App {...bootstrap} />, contentDiv)
+})
