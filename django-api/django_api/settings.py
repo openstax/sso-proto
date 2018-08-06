437312cb17f5d6f71c3069f5abe3015139f79ce1
Nathan Stitt <nathan@stitt.org>
2018-08-06 14:45:45 -0500

    add corsheaders

--- django-api/django_api/settings.py
+++ django-api/django_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,6 +82,12 @@ DATABASES = {
     }
 }
 
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8000',
+    'rdls.org'
+)
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
