7a7fb57aedaf60d898edc8d3966d69d890c32385
Dante Soares <dante.m.soares@rice.edu>
2018-09-21 15:45:01 -0500

    Simplified the decryption process for the shared cookie

--- sso-prototype/django-api/accounts/views.py
+++ sso-prototype/django-api/accounts/views.py
@@ -11,13 +11,13 @@ unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 
 class OXSessionDecryptor(object):
-    def __init__(self, secret_key_base, salt="encrypted cookie", keylen=64, iterations=1000):
-        self.secret = PBKDF2(secret_key_base, salt.encode(), keylen, iterations)
+    def __init__(self, shared_secret):
+        self.secret = shared_secret[:32]
 
     def get_cookie_data(self, cookie):
         cookie = base64.b64decode(urllib.parse.unquote(cookie).split('--')[0])
         encrypted_data, iv = map(base64.b64decode, cookie.split('--'.encode()))
-        cipher = AES.new(self.secret[:32], AES.MODE_CBC, iv)
+        cipher = AES.new(self.secret, AES.MODE_GCM, iv)
         return json.loads(unpad(cipher.decrypt(encrypted_data)))
 
 
@@ -26,6 +26,6 @@ def auth(request):
     if not cookie:
         return JsonResponse({ 'logged_in': False })
 
-    decrypt = OXSessionDecryptor(secret_key_base=settings.SHARED_SECRET)
+    decrypt = OXSessionDecryptor(shared_secret=settings.SHARED_SECRET)
     decrypted_user = decrypt.get_cookie_data(cookie)
     return JsonResponse(decrypted_user)
