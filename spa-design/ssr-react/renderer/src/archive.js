7a5764a4936de3437ec8d9262671c6e2bb6cf363
tom <tom.g.woodward@gmail.com>
2018-10-29 13:06:10 -0400

    more logging

--- spa-design/ssr-react/renderer/src/archive.js
+++ spa-design/ssr-react/renderer/src/archive.js
@@ -3,6 +3,13 @@ import fetch from 'node-fetch';
 const ARCHIVE_URL = process.env.ARCHIVE_URL;
 
 export const archiveLoader = id => {
+  const start = (new Date()).getTime();
+
   return fetch(ARCHIVE_URL + id)
-    .then(response => response.status === 200 ? response : Promise.reject(response));
+    .then(response => response.status === 200 ? response : Promise.reject(response))
+    .then(response => {
+      const end = (new Date()).getTime();
+      console.log(`fetched ${id} from archive in ${end - start}ms`);
+      return response;
+    });
 };
